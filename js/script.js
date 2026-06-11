9cdd9eb99d95a782e795c3736069a30afe65ce93
ridwanfirmansyahhhhn <105403688+ridwanfirmansyahhhhn@users.noreply.github.com>
2026-06-12 04:20:18 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,18 +4,38 @@ let i=0;
 function type(){if(i<msg.length){typing.innerHTML+=msg[i++];setTimeout(type,70)}}
 setTimeout(()=>{loader.style.display='none';type()},2200);
 
-startBtn.onclick=()=>{
-music.play();
-confetti({particleCount:300,spread:180});
-alert('🎉 Level 23 berhasil dibuka!');
-};
+startBtn.onclick=()=>{ music.play(); confetti({particleCount:300,spread:180}); alert('🎉 Selamat Ulang Tahun Bocil'); };
+function closeBirthdayModal(){
 
+    document
+    .getElementById("birthdayModal")
+    .classList
+    .add("hidden");
+
+}
 envelope.onclick=()=>letter.classList.toggle('hidden');
 
 giftBtn.onclick=()=>{
-confetti({particleCount:400,spread:220});
-alert('🎁 Voucher VIP: Aku Boleh Sama Kamu Trus 😌🤎');
+
+    confetti({
+        particleCount:400,
+        spread:220
+    });
+
+    document
+    .getElementById("giftModal")
+    .classList
+    .remove("hidden");
+
 };
+function closeGiftModal(){
+
+    document
+    .getElementById("giftModal")
+    .classList
+    .add("hidden");
+
+}
 
 setInterval(()=>{
 let h=document.createElement('div');
