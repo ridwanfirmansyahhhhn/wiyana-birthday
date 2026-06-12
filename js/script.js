8fdabaa8bdf8270cd4ce144dcfa655dedf382a77
ridwanfirmansyahhhhn <105403688+ridwanfirmansyahhhhn@users.noreply.github.com>
2026-06-12 13:54:26 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,13 +4,65 @@ let i=0;
 function type(){if(i<msg.length){typing.innerHTML+=msg[i++];setTimeout(type,70)}}
 setTimeout(()=>{loader.style.display='none';type()},2200);
 
-startBtn.onclick=()=>{ music.play(); confetti({particleCount:300,spread:180}); alert('🎉 Selamat Ulang Tahun Bocil'); };
+startBtn.onclick=()=>{
+
+    confetti({
+        particleCount:300,
+        spread:180
+    });
+
+    document
+    .getElementById("birthdayModal")
+    .classList
+    .remove("hidden");
+
+};
 function closeBirthdayModal(){
 
     document
     .getElementById("birthdayModal")
     .classList
     .add("hidden");
+
+    const music = document.getElementById("music");
+
+    music.play()
+    .catch(err => console.log(err));
+
+    // Confetti besar
+
+    confetti({
+        particleCount:500,
+        spread:250,
+        origin:{ y:0.6 }
+    });
+
+    // Confetti kiri
+
+    confetti({
+        particleCount:200,
+        angle:60,
+        spread:120,
+        origin:{ x:0 }
+    });
+
+    // Confetti kanan
+
+    confetti({
+        particleCount:200,
+        angle:120,
+        spread:120,
+        origin:{ x:1 }
+    });
+
+    // Efek zoom hero
+
+    document.querySelector(".hero").classList.add("celebrate");
+
+    // Scroll pelan ke bawah
+document.querySelector(".dance-left").style.display="block";
+document.querySelector(".dance-right").style.display="block";
+   
 
 }
 envelope.onclick=()=>letter.classList.toggle('hidden');
